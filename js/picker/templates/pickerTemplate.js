5518eb421fe08c6b64bf95f0ee9376d3cffbac4e
Bartosz <bartoszzborowski25@gmail.com>
2021-06-11 17:14:25 +0200

    Buil new css version

--- js/picker/templates/pickerTemplate.js
+++ js/picker/templates/pickerTemplate.js
@@ -11,7 +11,7 @@ const pickerTemplate = optimizeTemplateHTML(`<div class="v-datepicker">
       </div>
     </div>
     <div class="v-datepicker-main"></div>
-    <div class="datepicker-footer">
+    <div class="v-datepicker-footer">
       <div class="v-datepicker-controls">
         <button type="button" class="%buttonClass% today-btn"></button>
         <button type="button" class="%buttonClass% clear-btn"></button>
